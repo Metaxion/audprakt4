a4d8a19b63d3af1ade7b1012ee73c13d58060ab6
Marcel Thomas Krups <mk661756@fh-muenster.de>
2022-12-07 16:34:39 +0100

    Grundstruktur drin

--- main.c
+++ main.c
@@ -1,6 +1,61 @@
 #include <stdio.h>
+#include "stdlib.h"
+
+typedef struct baumknoten {
+    int ele1;
+    int ele2;
+    int ele3;
+    struct baumknoten *links;
+    struct baumknoten *mitte_links;
+    struct baumknoten *mitte_rechts;
+    struct baumknoten *rechts;
+} Knoten;
 
 int main() {
     printf("Hello, World!\n");
+
+    //init Baum mit 500 zufaelligen Elementen (jedes mal anderer Seed)
+
+    //Ausgeben in sortierter Reihenfolge (rekursive Traversierung)
+
+    //Ausgabe:
+    //Hoehe des entstandenen Baums
+    //Aus wie vielen Knoten besteht der Baum
+
+    //Loop
+    //Nutzer kann menugesteuert nach Elementen suchen
+    //Anzahl der dafuer notwendigen Vergleiche ausgeben
+
     return 0;
+}
+
+Knoten *initKnoten() {
+    Knoten *knot = calloc(sizeof(Knoten), 1);
+    knot->ele1 = -1;
+    knot->ele1 = -1;
+    knot->ele1 = -1;
+    knot->links = NULL;
+    knot->mitte_links = NULL;
+    knot->rechts = NULL;
+    knot->mitte_rechts = NULL;
+    return knot;
+}
+
+/**
+ * Init mit 500 Elementen
+ */
+void initBaum() {
+
+}
+
+void sortedInsert(int zahl) {
+
+}
+
+void ausgabe() {
+
+}
+
+void search() {
+
 }
